196261fb873b35dd21dce03362c12707c5a7f076
afiorg9000 <3533@holbertonschool.com>
2022-05-16 20:29:26 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
 
     with open("{}.csv".format(user), "w", newline="") as csvfile:
         my_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [my_writer.writerow([user, username, t.get("completed"), t.get("title")]) for t in tasks]
+        [my_writer.writerow([user, username, t.get("completed"),
+                            t.get("title")]) for t in tasks]
