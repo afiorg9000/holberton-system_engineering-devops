343f603cf7ee2822ef2ebb33ffcbc75862c8e30e
afiorg9000 <3533@holbertonschool.com>
2022-05-16 21:38:19 +0000

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import requests
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    user = argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users/'
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
+    username = requests.get(user_url + user).json().get('name')
+    tasks = requests.get(todos_url, params={'user': user}).json()
+
+    with open("{}.json".format(user), "w") as file:
+        json.dump({user: [{"task": t.get("title"),
+                  "completed": t.get("completed"),
+                   "username": username} for t in tasks]}, file)
