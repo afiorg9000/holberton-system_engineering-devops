62960ac8273330036c25ae8878bb7313b3ead70a
afiorg9000 <3533@holbertonschool.com>
2022-05-16 22:05:43 +0000

    indentation going crazy!

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Gather data from an API and export it into JSON format"""
+import requests
+import json
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "user").json()
+
+    with open("todo_all_employees.json", "w") as file:
+        json.dump({u.get("id"): [{"task": t.get("title"),
+                                  "completed": t.get("completed"),
+                                  "username": u.get("username")
+                                  }
+                                 for t in requests.get(
+                                     url + "todos",
+                                     params={"userId":
+                                             u.get("id")}).json()]
+                   for u in user}, file)
