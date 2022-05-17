7d73a36df61965e406e5b7b01284c36ffc915fe2
afiorg9000 <3533@holbertonschool.com>
2022-05-17 14:18:26 +0000

    chmod

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,13 +9,12 @@ if __name__ == "__main__":
     user_url = 'https://jsonplaceholder.typicode.com/users/'
     todos_url = 'https://jsonplaceholder.typicode.com/todos'
     username = requests.get(user_url + user).json().get('name')
-    tasks = requests.get(todos_url, params={'userId': user}).json()
+    tasks = requests.get(todos_url, params={'user': user}).json()
     completed = requests.get(todos_url,
-                             params={'userId': user,
+                             params={'user': user,
                                      'completed': 'true'}).json()
 
-    print("Employee", username, "is done with tasks(" +
-          str(len(completed)) + "/" + str(len(tasks)) + "):")
-
+    print("Employee {} is done with tasks({}/{}):".
+          format(username, str(len(completed)), str(len(tasks))))
     for i in completed:
         print("\t " + i['title'])
