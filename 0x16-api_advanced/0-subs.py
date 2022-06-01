ce82a66f2bd08badaba2bc4c151b20deff71df5e
afiorg9000 <3533@holbertonschool.com>
2022-06-01 15:45:47 -0400

    pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """queries the Reddit AP """
 import requests
 
@@ -7,8 +7,9 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    request = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-               headers={'User-Agent': 'Python/requests:APIproject:
-                        v1.0.0 (by /u/aaorrico23)'}).json()
-            subscribers = request.get("data", {}).get("subscribers", 0)
+    request = requests.get('http://www.reddit.com/r/{}/about.json'
+                           .format(subreddit),
+                           headers={'User-Agent': 'Python/requests: APIproject:
+                                    v1.0.0 (by / u/aaorrico23)'}).json()
+    subscribers = request.get("data", {}).get("subscribers", 0)
     return subscribers
